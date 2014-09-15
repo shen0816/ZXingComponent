86a901c2c9e0c5f4e52b36f46ac6a0b68df55159
Chuck <chuck@mobilogics.com.tw>
2014-09-15 14:18:01 +0800

    first

---
+++ app/src/main/java/tw/com/mobilogics/zxing/component/simple/MainActivity.java
@@ -0,0 +1,13 @@
+package tw.com.mobilogics.zxing.component.simple;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
